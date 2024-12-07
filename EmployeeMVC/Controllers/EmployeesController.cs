7fe0bc6e49a5855f489f93ed32099246dbf7485e
FatmaSaeed11 <113434113+FatmaSaeed11@users.noreply.github.com>
2024-12-08 01:39:53 +0200

    upload
    first commit

---
+++ EmployeeMVC/Controllers/EmployeesController.cs
@@ -0,0 +1,139 @@
+﻿using EmployeeMVC.Data;
+using EmployeeMVC.Models;
+using EmployeeMVC.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeMVC.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public EmployeesController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employees = await applicationDbContext.Employees.ToListAsync();
+            return View(employees);
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+         [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel model)
+        {
+
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = model.Name,
+                Email = model.Email,
+                Salary = model.Salary,
+                Department = model.Department,
+                DateOfBirth = model.DateOfBirth
+            };
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await applicationDbContext.Employees.AddAsync(employee);
+            await applicationDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        /*
+                [HttpPost]
+                public async Task<IActionResult> Create(CreateEmployeeViewModel createEmployeeRequest)
+                {
+                    var employee = new Employee()
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = createEmployeeRequest.Name,
+                        Email = createEmployeeRequest.Email,
+                        Salary = createEmployeeRequest.Salary,
+                        Department = createEmployeeRequest.Department,
+                        DateOfBirth = createEmployeeRequest.DateOfBirth
+                    };
+
+                    await applicationDbContext.Employees.AddAsync(employee);
+                    await applicationDbContext.SaveChangesAsync();
+
+                    return RedirectToAction("Index");
+                }
+
+
+                [HttpGet]
+                public IActionResult View(Guid id)
+                {
+                    var employee =  applicationDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+                    return View(employee);
+                }*/
+
+    
+
+        [HttpGet]
+        public async Task<IActionResult> View(Guid id)
+        {
+             var employee = await applicationDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+             if (employee != null)
+             {
+                var viewModel = new UpdateEmployeeViewModel()
+                {
+                            Id = employee.Id,
+                            Name = employee.Name,
+                            Email = employee.Email,
+                            Salary = employee.Salary,
+                            Department = employee.Department,
+                            DateOfBirth = employee.DateOfBirth                
+                };   
+                  return await Task.Run(()=>View("View",viewModel));
+             }
+                  return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> View(UpdateEmployeeViewModel model)
+        {
+            var employee = await applicationDbContext.Employees.FindAsync(model.Id);
+
+            if (employee != null)
+            {
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Salary = model.Salary;
+                employee.Department = model.Department;
+                employee.DateOfBirth = model.DateOfBirth;
+
+                await applicationDbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(UpdateEmployeeViewModel model)
+        {
+            var employee = await applicationDbContext.Employees.FindAsync(model.Id);
+            if (employee != null)
+            {
+                applicationDbContext.Employees.Remove(employee);
+                await applicationDbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EmployeeMVC/Models/Domain/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeMVC.Models.Domain
+{
+    public class Employee
+    {
+        [Required]
+         public Guid Id { get; set; }
+        [Required]
+         public string Name { get; set; }
+        [Required]
+         public string Email { get; set; }
+        [Required]
+         public long Salary { get; set; }
+        [Required]
+         public DateTime DateOfBirth { get; set; }
+        [Required]
+         public string Department { get; set; }
+
+    }
+}
